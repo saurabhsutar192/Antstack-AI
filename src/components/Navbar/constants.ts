84955b523fc8006e9b01dc9174fcd2deadf143a9
saurabhsutar192 <saurabhsutar192@gmail.com>
2023-11-20 11:28:07 +0530

    feat: initial commit

---
+++ src/components/Navbar/constants.ts
@@ -0,0 +1,7 @@
+export const navData = [
+  { name: "How We Do It", route: "#" },
+  { name: "Services", route: "#" },
+  { name: "Resources", route: "#" },
+  { name: "Products", route: "#" },
+  { name: "AntVerse", route: "#" },
+];
