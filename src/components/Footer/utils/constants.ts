c55defeb0a0984304883cf80b8ee7e8f0b0a3c5f
saurabhsutar192 <saurabhsutar192@gmail.com>
2023-11-21 18:53:36 +0530

    feat: adds footer and tunes benefits

---
+++ src/components/Footer/utils/constants.ts
@@ -0,0 +1,23 @@
+export const footerData: Record<string, { name: string; link: string }[]> = {
+  Services: [
+    { name: "Design", link: "#" },
+    { name: "UI Engineering", link: "#" },
+    { name: "Product Engineering", link: "#" },
+    { name: "Data Engineering", link: "#" },
+  ],
+  Industries: [
+    { name: "Healthcare", link: "#" },
+    { name: "Fin Tech", link: "#" },
+    { name: "Construction", link: "#" },
+    { name: "Logistics", link: "#" },
+    { name: "Social", link: "#" },
+  ],
+  "Quick Links": [
+    { name: "Case Studies", link: "#" },
+    { name: "Blog", link: "#" },
+    { name: "Events", link: "#" },
+    { name: "Careers", link: "#" },
+    { name: "Privacy Policy", link: "#" },
+    { name: "Contact Us", link: "#" },
+  ],
+};
