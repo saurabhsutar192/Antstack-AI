f9d5f41de9a202744171939ed68aafc16f6ef7b3
saurabhsutar192 <saurabhsutar192@gmail.com>
2023-11-22 13:15:36 +0530

    feat: adds caseStudies animations and refactor

---
+++ src/components/Benefits/utils/constants.ts
@@ -0,0 +1,17 @@
+export const benefitsData = [
+  {
+    image: null,
+    title: "Cost Efficiency",
+    desc: "Drastically reduce operational expenses and minimize wastage with streamlined, AI-driven processes.",
+  },
+  {
+    image: null,
+    title: "Scalability",
+    desc: "Expand with confidence. Our solutions are designed to scale in tandem with your growth aspirations.",
+  },
+  {
+    image: null,
+    title: "AI-Ready",
+    desc: "Our modernization services ensure that your tech stack isn't just new but AI-Ready, setting the stage for deeper insights and agile decision-making.",
+  },
+];
---
+++ src/components/CaseStudies/utils/constants.ts
@@ -0,0 +1,23 @@
+import reSkill from "@/assets/caseStudies/reSkill.png";
+import divo from "@/assets/caseStudies/divo.png";
+import ps from "@/assets/caseStudies/ps.png";
+import bfas from "@/assets/caseStudies/bfas.png";
+
+export const caseStudiesData = [
+  {
+    img: reSkill,
+    desc: "A Leading Financial App Builder from Indonesia Goes Serverless with AntStack",
+  },
+  {
+    img: divo,
+    desc: "A Leading Financial App Builder from Indonesia Goes Serverless with AntStack",
+  },
+  {
+    img: ps,
+    desc: "A Leading Financial App Builder from Indonesia Goes Serverless with AntStack",
+  },
+  {
+    img: bfas,
+    desc: "A Leading Financial App Builder from Indonesia Goes Serverless with AntStack",
+  },
+];
